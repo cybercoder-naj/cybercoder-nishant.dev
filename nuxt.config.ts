ecd5bf0c9a52edffbbb38c38d36a02d90a9942df
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-11-26 23:45:17 +0000

    changed compat date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4
-  }
+  },
+
+  compatibilityDate: "2024-11-26"
 })
