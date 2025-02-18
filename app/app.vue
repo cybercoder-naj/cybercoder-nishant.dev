d35bd0bb4549b6ab09f524f3b401b2781b03bb95
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-02-18 01:16:20 +0000

    added inspira ui with interactive grid pattern

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    Nishant's new website.
+    <InteractiveGridPattern
+      :class="[
+        '[mask-image:radial-gradient(350px_circle_at_center,white,transparent)]',
+        'inset-0 h-[200%] skew-y-12',
+      ]"
+    />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
+  modules: ['@nuxtjs/tailwindcss', 'motion-v/nuxt'],
   future: {
     compatibilityVersion: 4
   },
 
-  compatibilityDate: "2024-11-26"
+  compatibilityDate: "2024-11-26",
+
+  tailwindcss: {
+    viewer: false
+  }
 })
