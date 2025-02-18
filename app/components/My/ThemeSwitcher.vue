13e490a60dca846fc358fc1655662defef39bd35
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-02-18 04:07:55 +0000

    Finished toggle with animations

--- app/components/My/ThemeSwitcher.vue
+++ app/components/My/ThemeSwitcher.vue
@@ -7,41 +7,53 @@ const lightTheme = defineModel<boolean>();
 <template>
   <Switch
     v-model="lightTheme"
-    class="h-6 w-12 rounded-full bg-gradient-to-b"
+    class="m-5 h-6 w-12 rounded-full bg-gradient-to-b"
     :class="{
       'from-blue-400 via-blue-200 to-blue-400': lightTheme,
       'from-slate-800 via-slate-600 to-slate-800': !lightTheme
     }">
-    <div v-if="lightTheme" class="flex h-full items-center px-0.5 text-white">
-      <Icon name="material-symbols:circle" class="size-5" />
-      <div class="relative h-full flex-1">
+    <div class="relative flex h-full items-center px-0.5 text-white">
+      <!-- Sun and moon icons -->
+      <Icon
+        :name="
+          lightTheme ? 'material-symbols:circle' : 'material-symbols:dark-mode'
+        "
+        class="size-5 -scale-x-100 transition-transform duration-300 ease-in-out"
+        :class="{ 'translate-x-6': !lightTheme }" />
+
+      <!-- Stars and circles -->
+      <div class="h-full flex-1">
+        <!-- Light themed decoration -->
         <Icon
           name="material-symbols:circle"
-          class="absolute left-0.5 top-1/2 size-1" />
+          class="absolute left-[58%] top-1/2 size-1 transition-opacity delay-0 duration-300 ease-in-out"
+          :class="lightTheme ? 'opacity-100' : 'opacity-0'" />
         <Icon
           name="material-symbols:circle"
-          class="absolute right-1 top-1 size-2" />
-      </div>
-    </div>
-    <div v-else class="flex h-full items-center px-0.5 text-white">
-      <div class="relative h-full flex-1">
+          class="absolute right-1 top-1 size-2 transition-opacity delay-0 duration-300 ease-in-out"
+          :class="lightTheme ? 'opacity-100' : 'opacity-0'" />
+
+        <!-- Dark themed decoration -->
         <Icon
           name="material-symbols-light:star"
-          class="absolute left-0.5 top-0 size-3" />
+          class="absolute left-1 top-0.5 size-3 transition-opacity delay-0 duration-300 ease-in-out"
+          :class="!lightTheme ? 'opacity-100' : 'opacity-0'" />
 
         <Icon
           name="material-symbols-light:star"
-          class="absolute bottom-0 right-0 size-3" />
+          class="absolute bottom-0 left-1/4 size-3 transition-opacity delay-0 duration-300 ease-in-out"
+          :class="!lightTheme ? 'opacity-100' : 'opacity-0'" />
 
         <Icon
           name="material-symbols-light:circle"
-          class="absolute -right-0.5 top-1 size-1.5" />
+          class="absolute right-[45%] top-1 size-1.5 transition-opacity delay-0 duration-300 ease-in-out"
+          :class="!lightTheme ? 'opacity-100' : 'opacity-0'" />
 
         <Icon
           name="material-symbols-light:circle"
-          class="absolute bottom-1.5 left-1 size-1" />
+          class="absolute bottom-1.5 left-1 size-1 transition-opacity delay-0 duration-300 ease-in-out"
+          :class="!lightTheme ? 'opacity-100' : 'opacity-0'" />
       </div>
-      <Icon name="material-symbols:dark-mode" class="size-5 -scale-x-100" />
     </div>
   </Switch>
 </template>
