6624228b2f59abfd80f3bc8a10f98913fbdfd107
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-02-18 01:23:57 +0000

    formatted some code

--- app/app.vue
+++ app/app.vue
@@ -3,8 +3,7 @@
     <InteractiveGridPattern
       :class="[
         '[mask-image:radial-gradient(350px_circle_at_center,white,transparent)]',
-        'inset-0 h-[200%] skew-y-12',
-      ]"
-    />
+        'inset-0 h-[200%] skew-y-12'
+      ]" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,9 @@ export default defineNuxtConfig({
     compatibilityVersion: 4
   },
 
-  compatibilityDate: "2024-11-26",
+  compatibilityDate: '2024-11-26',
 
   tailwindcss: {
     viewer: false
   }
-})
+});
