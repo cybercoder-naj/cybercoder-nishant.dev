1ca12f8f5e6e9aa30f8139bc3b29cec59ab67e2c
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-07-06 13:19:55 +0100

    added text to about screen

--- app/components/Section/Intro.vue
+++ app/components/Section/Intro.vue
@@ -3,26 +3,40 @@ import gsap from 'gsap';
 import SplitText from 'gsap/SplitText';
 
 const cybercoder = useTemplateRef<HTMLSpanElement>('cybercoder');
+const description = useTemplateRef<HTMLSpanElement>('description');
+
 onMounted(() => {
+  const tl = gsap.timeline();
+
   const split = SplitText.create(cybercoder.value, {
     type: 'chars'
   });
 
-  gsap.from(split.chars, {
+  tl.from(split.chars, {
     duration: 0.5,
     opacity: 0,
     y: 50,
     stagger: 0.03
   });
+
+  tl.from(
+    description.value,
+    {
+      duration: 0.5,
+      opacity: 0,
+      y: 25
+    },
+    '-=0.4'
+  );
 });
 </script>
 
 <template>
   <section class="relative min-h-dvh w-full">
     <div
-      class="2xs:text-4xl relative z-20 flex min-h-dvh flex-col items-start justify-center text-3xl sm:text-5xl md:text-6xl">
-      <h1 class="font-semibold">Hi, I'm Nishant</h1>
-      <div class="mt-2">
+      class="relative z-20 flex min-h-dvh w-1/2 flex-col items-start justify-center">
+      <h1 class="heading-1 font-semibold">Hi, I'm Nishant</h1>
+      <div class="heading-1">
         <div
           class="text-primary mt-2 inline max-w-min"
           ref="cybercoder"
@@ -30,10 +44,10 @@ onMounted(() => {
           AKA Cybercoder
         </div>
       </div>
-    </div>
-
-    <div class="min-h-dvh">
-      <h3 class="2xs:text-2xl text-3xl sm:text-5xl md:text-6xl">I am a:</h3>
+      <p class="heading-6 mt-8" ref="description">
+        I'm a Developer, Student, Musician and Teacher. I also enjoy cycling,
+        motorsport racing and coffee.
+      </p>
     </div>
 
     <canvas class="absolute inset-0 z-10 hidden h-full w-full"> </canvas>
--- app/plugins/gsap.client.ts
+++ app/plugins/gsap.client.ts
@@ -1,8 +1,7 @@
 import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
 import SplitText from 'gsap/SplitText';
 import MorphSVGPlugin from 'gsap/MorphSVGPlugin';
 
 export default defineNuxtPlugin(() => {
-  gsap.registerPlugin(ScrollTrigger, SplitText, MorphSVGPlugin);
+  gsap.registerPlugin(SplitText, MorphSVGPlugin);
 });
